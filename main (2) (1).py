6b7f16a2aac76c7d6858c0b52fbef67c966c72c4
comrade <105161490+AlmazUlugbekov@users.noreply.github.com>
2024-09-30 09:42:21 +0600

    Update main (2) (1).py

--- main (2) (1).py
+++ main (2) (1).py
@@ -1,7 +1,7 @@
 from os import system
 
-a = round(int(input('Write the first number: ')))
-b = round(int(input('Write the second number: ')))
+first= round(int(input('Write the first number: ')))
+second = round(int(input('Write the second number: ')))
 operators = input('Choose operations: \n'
                   "1 - Multiplication \n"
                   "2 - Divide \n"
@@ -9,21 +9,21 @@ operators = input('Choose operations: \n'
                   "4 - Addition \n"
                  "Write the operation: ")
 if(operators == '4'):
-    _ = system('cls')
-    sum = a + b
-    print(f'{a} + {b} = {sum}')
+    clearTerminal = system('cls')
+    sum = first + second
+    print(f'{first} + {second} = {sum}')
 elif(operators == '3'):
-    _ = system('cls')
-    sum = a - b
-    print(f'{a} - {b} = {sum}')
+    clearTerminal = system('cls')
+    sum = first - second
+    print(f'{first} - {second} = {sum}')
 elif(operators == '1'):
-    _ = system('cls')
-    sum = a * b
-    print(f'{a} * {b} = {sum}')
+    clearTerminal = system('cls')
+    sum = first * second
+    print(f'{first} * {second} = {sum}')
 elif(operators == '2'):
-    _ = system('cls')
-    sum = a / b
-    print(f'{a} / {b} = {sum}')
+    clearTerminal = system('cls')
+    sum = first / second
+    print(f'{first} / {second} = {sum}')
 else:
-    _ = system('cls')
+    clearTerminal = system('cls')
     print('Error')
